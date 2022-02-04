1190054f951e7b92c85de26818bab7ae1eddc502
khaledk2 <khaledk2@hotmail.com>
2022-02-04 09:30:15 +0000

    Fix type and improve message

--- omero_search_client/main/utils.py
+++ omero_search_client/main/utils.py
@@ -128,9 +128,7 @@ def determine_search_results(query_):
         res= seracrh_query(query, resource, bookmark , all_main_attributes.get(resource))
         if res.get("error"):
             return json.dumps(res)
-        if not res.get("results"):
-            res["Error"] = "Something went wrong, please try again later"
-        elif len(res["results"]) == 0:
+        if not res.get("results") or len(res["results"]) == 0:
             res["Error"] = "Your query returns no results"
             return res
         if len (queries_to_send)==1:
@@ -150,11 +148,8 @@ def determine_search_results(query_):
 
 def process_search_results(results, resource, columns_def):
     returned_results={}
-    if not results.get("results"):
-        returned_results["Error"] = "Something went wrong, please try again later"
-        return returned_results
-    elif len(results["results"])==0:
-        returned_results["Error"]="Your query returns no results"
+    if not results.get("results") or len(results["results"])==0:
+        returned_results["Error"] = "Your query returns no results"
         return returned_results
 
     cols=[]
