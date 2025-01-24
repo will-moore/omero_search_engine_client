a3a23c2b9d3201ab7bcd8b53382ebf3408569d32
William Moore <w.moore@dundee.ac.uk>
2025-01-24 16:05:02 +0000

    use adapter-static to build static site to /build

--- src/routes/+layout.js
+++ src/routes/+layout.js
@@ -2,3 +2,6 @@
 // Turn off ServerSideRendering for the whole app.
 // Makes it into a purely client-side SPA.
 export const ssr = false;
+
+// See https://svelte.dev/docs/kit/adapter-static#Options
+export const prerender = true;
